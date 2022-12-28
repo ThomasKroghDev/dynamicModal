5b391b55f413c465c5b4956d0da97f8015f0ec92
Thomas <thomas.krogh.dev@gmail.com>
2022-12-27 20:20:52 -0600

    working modals

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const showModalbtns = document.querySelectorAll('.open-modal');
+const modalHeader = document.querySelector('.modalHeader');
+const modalContent = document.querySelector('.modalContent');
+
+const closebtn = document.querySelector('.closeBtn');
+
+const toggle = () => {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+for (let i = 0; i < showModalbtns.length; i++) {
+  showModalbtns[i].addEventListener('click', toggle);
+}
+
+closebtn.addEventListener('click', toggle);
+overlay.addEventListener('click', toggle);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    toggle();
+  }
+});
